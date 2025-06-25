2af8f75b16881bbb3fc282e9b61a4bd6cc13abc4
Patryk Wisniewski <pwisniewski1991@gmail.com>
2025-06-25 22:08:42 +0200

    feat: implement LoginPage component and update App to use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
 	return (
 		<>
-			<HomePage />
+			<LoginPage />
 		</>
 	);
 }
